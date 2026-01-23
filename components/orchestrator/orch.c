129a3383b58ad64632555b66b755dcee0e35f8f5
IvanHdzF <ivan083198@hotmail.com>
2026-01-23 07:42:41 -0700

    feat: Initial files creations and linkage (WIP)

---
+++ components/orchestrator/orch.c
@@ -0,0 +1,12 @@
+#include "orch.h"
+#include "retrofit_os_types.h"
+
+os_err_t orchestrator_init(void)
+{
+    return OS_OK;
+}
+
+os_err_t orchestrator_process(const os_evt_t *evt)
+{
+    return OS_OK;
+}
